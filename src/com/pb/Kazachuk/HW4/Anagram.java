ec3c50089298a15c4b89ce7dc5ac53dacc4e82c3
Oleksii Kazachuk <zp080406kaa@gmail.com>
2021-10-31 22:24:29 +0300

    CapitalLetter

---
+++ src/com/pb/Kazachuk/HW4/Anagram.java
@@ -0,0 +1,17 @@
+package com.pb.Kazachuk.HW4;
+
+import java.util.Scanner;
+
+public class Anagram {
+    public static void main(String[] arg) {
+        String x;
+        String y;
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input your phrase 1: ");
+        x = in.next();
+        System.out.print("Input your phrase 2 - anagram: ");
+        y = in.next();
+    }
+
+}
--- src/com/pb/Kazachuk/HW4/CapitalLetter.java
+++ src/com/pb/Kazachuk/HW4/CapitalLetter.java
@@ -9,19 +9,37 @@ public class CapitalLetter {
         String x;
         Scanner in = new Scanner(System.in);
         System.out.print("Input your phrase: ");
-        x = in.next();
-        System.out.println(toUP(x));
+        x = in.nextLine();
+
+        System.out.println("First variant " + toUp(x));
+
+        toUpSecond(x);
     }
-    public static StringBuilder toUP(String x) {
+
+    public static StringBuilder toUp(String x) {
         char[] result = x.toCharArray();
         if (Character.isAlphabetic(result[0])) result[0] = Character.toUpperCase(result[0]);
         StringBuilder res = new StringBuilder("" + result[0]);
         for (int i=1; i<result.length; i++)
         {
-            if (Character.isAlphabetic(result[i]) && !Character.isAlphabetic(result[i-1]))result[i]=Character.toUpperCase(result[i]);
+            if (Character.isAlphabetic(result[i])
+                    && !Character.isAlphabetic(result[i-1]))result[i]=Character.toUpperCase(result[i]);
             res.append(result[i]);
         }
         return res;
+    }
+
+    public static void toUpSecond(String x) {
+        String output = "";
+        String[] words = x.split(" ", 100);
+
+        for (String word:words) {
+            String first = word.substring(0, 1).toUpperCase();
+            String all = word.substring(1);
+            output += (first + all + " ");
+        }
+
+        System.out.print("Second variant " + output);
     }
 }
 
